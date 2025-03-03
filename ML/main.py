cf0b9b557be7e548f558165492047d484d2c6986
Ahenrie <10914763@uvu.edu>
2025-03-03 15:26:38 -0700

    Cleaned up faking program to relect user analytics

--- ML/main.py
+++ ML/main.py
@@ -2,9 +2,10 @@ from cleaner import CleanedDF
 from pikl_maker import IsolationForestModel
 
 if __name__ == "__main__":
-    cleaned_data = CleanedDF('test.csv')
+    cleaned_data = CleanedDF('fakelogs.csv')
     cleaned_data.preprocessing()
     cleaned_data.printInfo()
+    print(cleaned_data.df["Exe"].unique())
 
     newModel = IsolationForestModel(cleaned_data.df)
     newModel.train()
